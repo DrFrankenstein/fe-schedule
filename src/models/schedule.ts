466389a00e6652bbfb21d1bdfcd74e01e297556c
Dr Frankenstein <doktorfrankenstein@gmail.com>
2023-03-03 16:41:50 -0500

    fix(models): speakers go somewhere

--- src/models/schedule.ts
+++ src/models/schedule.ts
@@ -1,9 +1,11 @@
 import type { CategoryField } from "./category";
 import type { Room } from "./room";
 import type { Session } from "./session";
+import type { Speaker } from "./speaker";
 
 export interface Schedule {
     sessions: Session[];
     categories: CategoryField[];
+    speakers: Speaker[];
     rooms: Room[];
 }
