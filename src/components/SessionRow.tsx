e9dee6bed88f3c9bd7a5058bc5faff6b9cb38c5a
Dr Frankenstein <doktorfrankenstein@gmail.com>
2023-03-05 16:55:10 -0500

    feat: display times, sort of

--- src/components/SessionRow.tsx
+++ src/components/SessionRow.tsx
@@ -5,10 +5,18 @@ export interface SessionRowProps {
     session: Session;
 }
 
+export const SessionHeader: React.FC = props => <thead>
+    <th>Start</th>
+    <th>End</th>
+    <th>Title</th>
+</thead>;
+
 export const SessionRow: React.FC<SessionRowProps> = props => {
     const { session } = props;
 
     return <tr>
+        <td>{session.startsAt}</td>
+        <td>{session.endsAt}</td>
         <td>{session.title}</td>
     </tr>;
 }
--- src/components/SessionTable.tsx
+++ src/components/SessionTable.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Session } from "../models/session";
-import { SessionRow } from "./SessionRow";
+import { SessionHeader, SessionRow } from "./SessionRow";
 
 export interface SessionTableProps {
     sessions: Session[];
@@ -8,6 +8,7 @@ export interface SessionTableProps {
 
 export const SessionTable: React.FC<SessionTableProps> = props => {
     return <table>
+        <SessionHeader />
         {props.sessions.map(session => <SessionRow key={session.id} session={session} />)}
     </table>
 }
