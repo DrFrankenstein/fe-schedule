d44e5b29d3f09a32193adacf76518898aa36e8fe
Dr Frankenstein <doktorfrankenstein@gmail.com>
2023-03-05 20:07:06 -0500

    feat: split events and services

--- src/pages/EventsSchedule.tsx
+++ src/pages/EventsSchedule.tsx
@@ -1,24 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useMemo } from "react";
 import { SessionTable } from "../components/SessionTable";
 import { EventContext } from "../context/EventContext";
+import type { Schedule } from "../models/schedule";
 import { useSchedule } from "../queries/schedule";
+
+const getEvents = (schedule?: Schedule) =>
+    schedule?.sessions.filter(session => !session.isServiceSession);
 
 export const EventsSchedule: React.FC = () => {
     const domain = useContext(EventContext);
     const scheduleQuery = useSchedule(domain);
+    const schedule = scheduleQuery.data;
+    const sessions = useMemo(() => getEvents(schedule), [schedule]);
 
     if (scheduleQuery.isLoading) {
         return <div>Loading&hellip;</div>;
     }
 
-    if (scheduleQuery.isError) {
+    if (scheduleQuery.isError || sessions === undefined) {
         return <div>oh no :(</div>;
     }
 
-    const schedule = scheduleQuery.data;
-
     return <>
         <h1>Sessions:</h1>
-        <SessionTable sessions={schedule.sessions} />
+        <SessionTable sessions={sessions} />
     </>;
 };
--- src/pages/ServicesSchedule.tsx
+++ src/pages/ServicesSchedule.tsx
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useContext, useMemo } from "react";
+import { SessionTable } from "../components/SessionTable";
+import { EventContext } from "../context/EventContext";
+import type { Schedule } from "../models/schedule";
+import { useSchedule } from "../queries/schedule";
+
+const getServicesSchedule = (schedule?: Schedule) =>
+    schedule?.sessions.filter(session => session.isServiceSession);
 
 export const ServicesSchedule: React.FC = () => {
-    return <></>;
-}
+    const domain = useContext(EventContext);
+    const scheduleQuery = useSchedule(domain);
+    const schedule = scheduleQuery.data;
+    const sessions = useMemo(() => getServicesSchedule(schedule), [schedule]);
+
+    if (scheduleQuery.isLoading) {
+        return <div>Loading&hellip;</div>;
+    }
+
+    if (scheduleQuery.isError || sessions === undefined) {
+        return <div>oh no :(</div>;
+    }
+
+    return <>
+        <h1>Services:</h1>
+        <SessionTable sessions={sessions} />
+    </>;
+};
