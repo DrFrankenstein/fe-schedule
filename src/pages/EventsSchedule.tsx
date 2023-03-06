3b4f6ca733ca2d20bab61363b114503ca6514988
Dr Frankenstein <doktorfrankenstein@gmail.com>
2023-03-05 22:18:57 -0500

    feat: group events by date

--- src/pages/EventsSchedule.tsx
+++ src/pages/EventsSchedule.tsx
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React, { useContext, useMemo } from "react";
+import groupBy from "lodash.groupby";
 import { SessionTable } from "../components/SessionTable";
 import { EventContext } from "../context/EventContext";
 import { useEvents } from "../queries/schedule";
@@ -6,18 +7,25 @@ import { useEvents } from "../queries/schedule";
 export const EventsSchedule: React.FC = () => {
     const domain = useContext(EventContext);
     const eventsQuery = useEvents(domain);
-    const events = eventsQuery.events;
+    const events = eventsQuery.events ?? [];
+    const eventsByDate = useMemo(
+        () => groupBy(events, event => new Date(event.startsAt).toDateString()),
+        [events]
+    );
 
     if (eventsQuery.isLoading) {
         return <div>Loading&hellip;</div>;
     }
 
-    if (eventsQuery.isError || events === undefined) {
+    if (eventsQuery.isError) {
         return <div>oh no :(</div>;
     }
 
     return <>
         <h1>Sessions:</h1>
-        <SessionTable sessions={events} />
+        {Object.entries(eventsByDate).map(([date, dateEvents]) => <div key={date}>
+            <h2>{date}</h2>
+            <SessionTable sessions={dateEvents} />
+        </div>)}
     </>;
 };
